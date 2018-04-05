6d2def7ece3877f6d7d34b7936215f3307f49b85
KDH <KDH@DESKTOP-VCNR5OM>
2018-04-05 21:23:36 +0900

    Frist

--- src/dev/kdh/ani/MainClass.java
+++ src/dev/kdh/ani/MainClass.java
@@ -5,6 +5,6 @@ public class MainClass {
 	public static void main(String[] args){
 		// TODO Auto-generated method stub
 		System.out.println("Hello java");
-		System.out.println("overwatch,Slay the spire");
+		System.out.println("overwatch,Slay the spire,Portal");
 	}
 }

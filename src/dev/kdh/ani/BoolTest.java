b0a854b776197acb5227c45cae306b0b8edbb270
KDH <KDH@DESKTOP-VCNR5OM>
2018-04-05 22:00:47 +0900

    Secend

---
+++ src/dev/kdh/ani/BoolTest.java
@@ -0,0 +1,13 @@
+package dev.kdh.ani;
+
+public class BoolTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        boolean aVar=true;
+        boolean bVar=false;
+        System.out.println(aVar);
+        System.out.println(bVar);
+	}
+
+}
---
+++ src/dev/kdh/ani/HakJum.java
@@ -0,0 +1,27 @@
+package dev.kdh.ani;
+
+import javax.swing.JOptionPane;
+
+public class HakJum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int Jumsu ;
+        char HakJum ;
+        Jumsu=Integer.parseInt(JOptionPane.showInputDialog("점수입력: "));
+        		
+        if(Jumsu >=90){
+           HakJum = 'A';
+        }else if(Jumsu >=80){
+        	    HakJum ='B';
+        }else if(Jumsu >=70){
+        	    HakJum = 'C';
+        }else if(Jumsu >=60){
+        	    HakJum = 'D';
+        }else{
+        	HakJum = 'F';
+        }
+           
+    System.out.println("취득한 학점은 "+ HakJum +"입니다." );
+	}
+}

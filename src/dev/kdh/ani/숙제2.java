50ddb905a70943852e88d351387b2fc6470246ac
KDH <KDH@DESKTOP-VCNR5OM>
2018-04-06 22:01:29 +0900

    세번째

--- src/dev/kdh/ani/숙제2.java
+++ src/dev/kdh/ani/숙제2.java
@@ -6,9 +6,9 @@ public class 숙제2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int 원주율 ;
+		double 원주율 ;
         char aVar ;
-        원주율 =Integer.parseInt(JOptionPane.showInputDialog("숫자입력: "));
+        원주율 =Double.parseDouble(JOptionPane.showInputDialog("숫자입력: "));
         
         
         System.out.println("원주율은"+ 원주율 +"입니다." );
